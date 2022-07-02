8e98074a54eced8cc21683f9ef86a999efffc841
Martian Jeirgif <mnsh.exe@gmail.com>
2022-07-02 17:24:49 +0000

    File UI fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'ng-ui';
   response: any = '';
+  api: string = 'https://m9j-java-learn-7wj5465p2rpx7-8080.githubpreview.dev/';
+  fileList: any[] = [];
 
   constructor(private http: HttpClient) {}
-
   ngOnInit(): void {
-    this.http
-      .get<any>(
-        'https://m9j-java-learn-7wj5465p2rpx7-8080.githubpreview.dev/greeting'
-      )
-      .subscribe((response) => {
-        this.response = JSON.stringify(response);
-      });
+    this.getAllFiles();
+  }
+  getGreeting() {
+    this.http.get<any>(this.api + 'greeting').subscribe((response) => {
+      console.log(response);
+      this.response = response;
+    });
+  }
+
+  getAllFiles() {
+    this.http.get<any>(this.api + 'file').subscribe((response) => {
+      console.log(response);
+      if (response) {
+        this.fileList = response;
+      }
+    });
+  }
+
+  newFile() {
+    const nextFileNumber = this.fileList.length + 1;
+    const nextFile = {
+      fileName: 'Untitled-' + nextFileNumber,
+      fileContent: "",
+      isSaving: false,
+    };
+    this.fileList.unshift(nextFile);
+    nextFile.isSaving = true;
+    this.http.post(this.api + `file`, nextFile).subscribe(response=> {
+      if (response) nextFile.isSaving = false;
+    })
   }
 }
