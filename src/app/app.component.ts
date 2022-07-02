95c3c6e804dd72bbcf46069bbe18a82071b33877
Martian Jeirgif <mnsh.exe@gmail.com>
2022-07-02 09:38:36 +0000

    Hello world rest api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-ui';
+  response: any = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<any>(
+        'https://m9j-java-learn-7wj5465p2rpx7-8080.githubpreview.dev/greeting'
+      )
+      .subscribe((response) => {
+        this.response = JSON.stringify(response);
+      });
+  }
 }
