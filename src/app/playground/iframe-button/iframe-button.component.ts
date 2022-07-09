9ac1d344898120ecf527a43541f543be364c4687
Martian Jeirgif <mnsh.exe@gmail.com>
2022-07-09 07:08:31 +0000

    removal of unnecessary template files

--- src/app/playground/iframe-button/iframe-button.component.ts
+++ src/app/playground/iframe-button/iframe-button.component.ts
@@ -3,13 +3,44 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-iframe-button',
   templateUrl: './iframe-button.component.html',
-  styleUrls: ['./iframe-button.component.scss']
+  styleUrls: ['./iframe-button.component.scss'],
 })
 export class IframeButtonComponent implements OnInit {
+  isIframeEnabled: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showIframe() {
+    this.isIframeEnabled = true;
+
+    this.next(() => {
+      const iframe: HTMLIFrameElement = document.getElementById(
+        'bopFrame'
+      ) as HTMLIFrameElement;
+      const content: any = iframe.contentWindow || iframe.contentDocument;
+      const iframeDocument = content.document;
+      if (iframeDocument) {
+        const submitButton = iframeDocument.createElement('button');
+        submitButton.id = 'submitButton';
+        submitButton.innerHTML = 'Hide this';
+        submitButton.addEventListener('click', () => {
+          this.isIframeEnabled = false;
+          // iframe.setAttribute('src', '');
+          // iframe.setAttribute('hidden', 'true');
+        });
+        const iframeBody = iframeDocument.body;
+        iframeBody.appendChild(submitButton);
+        console.log('appended');
+      }
+    });
   }
 
+  next(fn: Function) {
+    let tmr = setTimeout(() => {
+      clearTimeout(tmr);
+      fn();
+    }, 250);
+  }
 }
