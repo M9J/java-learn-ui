24569aee2ed56938053a73049ad31935bac1a0c4
Martian Jeirgif <mnsh.exe@gmail.com>
2022-07-09 15:46:01 +0000

    iframe button logic fixes

--- src/app/playground/iframe-button/iframe-button.component.ts
+++ src/app/playground/iframe-button/iframe-button.component.ts
@@ -1,46 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-iframe-button',
   templateUrl: './iframe-button.component.html',
   styleUrls: ['./iframe-button.component.scss'],
 })
-export class IframeButtonComponent implements OnInit {
+export class IframeButtonComponent implements OnInit, OnDestroy {
   isIframeEnabled: boolean = false;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    window.addEventListener('message', this.onMessageHandler.bind(this));
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('message', this.onMessageHandler.bind(this));
+  }
+
+  onMessageHandler(e: any) {
+    if (e.data === 'hideIframe') {
+      this.isIframeEnabled = false;
+    }
+  }
 
   showIframe() {
     this.isIframeEnabled = true;
-
     this.next(() => {
       const iframe: HTMLIFrameElement = document.getElementById(
         'bopFrame'
       ) as HTMLIFrameElement;
-      const content: any = iframe.contentWindow || iframe.contentDocument;
-      const iframeDocument = content.document;
-      if (iframeDocument) {
-        const submitButton = iframeDocument.createElement('button');
-        submitButton.id = 'submitButton';
-        submitButton.innerHTML = 'Hide this';
-        submitButton.addEventListener('click', () => {
-          this.isIframeEnabled = false;
-          // iframe.setAttribute('src', '');
-          // iframe.setAttribute('hidden', 'true');
-        });
-        const iframeBody = iframeDocument.body;
-        iframeBody.appendChild(submitButton);
-        console.log('appended');
-      }
-    });
+      iframe.onload = () => {
+        console.log('iframe loaded');
+        const content: any = iframe.contentWindow || iframe.contentDocument;
+        const iframeDocument = content.document;
+        if (iframeDocument) {
+          const submitButton = iframeDocument.createElement('button');
+          submitButton.id = 'submitButton';
+          submitButton.innerHTML = 'Hide this';
+          submitButton.addEventListener('click', () => {
+            console.log('submitButton: clicked');
+            window.parent.postMessage('hideIframe', '*');
+          });
+          const iframeBody = iframeDocument.body;
+          iframeBody.appendChild(submitButton);
+          console.log('appended');
+        }
+      };
+    }, 500);
   }
 
-  next(fn: Function) {
+  next(fn: Function, ms: number) {
     let tmr = setTimeout(() => {
       clearTimeout(tmr);
       fn();
-    }, 250);
+    }, ms);
   }
 }
