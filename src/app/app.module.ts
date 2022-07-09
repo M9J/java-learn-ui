2c018bc3755226cf6bed086054a65ff023d895a3
Martian Jeirgif <mnsh.exe@gmail.com>
2022-07-09 05:52:46 +0000

    playground, iframe button poc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlaygroundComponent } from './playground/playground.component';
+import { IframeButtonComponent } from './playground/iframe-button/iframe-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaygroundComponent,
+    IframeButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/playground/iframe-button/iframe-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iframe-button',
+  templateUrl: './iframe-button.component.html',
+  styleUrls: ['./iframe-button.component.scss']
+})
+export class IframeButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
